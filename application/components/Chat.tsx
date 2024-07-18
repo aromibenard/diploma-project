d8a704ac4ee90e12d95ac391f960b9d337de896c
Benard Aromi <112564277+aromibenard@users.noreply.github.com>
2024-07-18 22:42:54 +0000

    footer: complete

--- application/components/Chat.tsx
+++ application/components/Chat.tsx
@@ -5,6 +5,8 @@ import { Message, continueConversation } from '@/app/actions';
 import { readStreamableValue } from 'ai/rsc';
 import { Button, Input, Paper } from '@mui/material';
 import { Send } from '@mui/icons-material';
+import { Tabs, TabsContent, TabsList, TabsTrigger } from './ui/tabs';
+import { Illustrations } from './Illustrations';
 
 
 export default function Chat() {
@@ -22,57 +24,67 @@ export default function Chat() {
 
 
   return (
-    <div className={'flex flex-col p-4 h-screen'}>
-      <h1 className='m-2 px-2 font-semibold text-xl text-gray-700'>Describe Animal's condition below:</h1>
-      <Paper className='flex-1 flex-col overflow-y-auto p-4 mb-4'
-        elevation={3}
-      >
-          {conversation.map((message, index) => (
-            <div key={index} 
-              className={message.role === 'user' ? 
-               'p-3 px-4 justify-end bg-purple-500 inline-block rounded-xl my-2 shadow-md text-white' 
-               : 'p-3 px-4 justify-start mb-4 bg-gray-200 my-2 rounded-xl shadow-md'}>
-              {message.role}: {message.content}
+    <div className={'flex flex-col p-4 my-3'}>
+      <Tabs defaultValue="chat" className="w-full h-full" color='purple'>
+          <TabsList>
+            <TabsTrigger value="chat">First Aid</TabsTrigger>
+            <TabsTrigger value="illustration">Illustrations</TabsTrigger>
+          </TabsList> 
+          <TabsContent value="chat">  
+            <h1 className='m-2 px-2 font-semibold text-xl text-gray-700'>Describe Animal's condition below:</h1>
+            <Paper className='flex-1 flex-col overflow-y-auto p-4 mb-4'
+              elevation={3}
+            >
+                {conversation.map((message, index) => (
+                  <div key={index} 
+                    className={message.role === 'user' ? 
+                    'p-3 px-4 justify-end bg-purple-500 inline-block rounded-xl my-2 shadow-md text-white' 
+                    : 'p-3 px-4 justify-start mb-4 bg-gray-200 my-2 rounded-xl shadow-md'}>
+                    {message.role}: {message.content}
+                  </div>
+                ))}
+                <div ref={messagesEndRef} />
+            </Paper>
+            <div className='flex w-full space-x-4 p-4'>
+              <Input
+                placeholder="Describe injury or accident"
+                type="text"
+                value={input}
+                onChange={event => {
+                  setInput(event.target.value);
+                }}
+                className='flex-1 mr-2'
+              />
+              <Button
+                variant="outlined"
+                className='bg-purple-400'
+                onClick={async () => {
+                  const { messages, newMessage } = await continueConversation([
+                    ...conversation,
+                    { role: 'user', content: input },
+                  ]);
+
+                  let textContent = '';
+
+                  for await (const delta of readStreamableValue(newMessage)) {
+                    textContent = `${textContent}${delta}`;
+
+                    setConversation([
+                      ...messages,
+                      { role: 'assistant', content: textContent },
+                    ]);
+                  }
+                  setInput('')
+                }}
+              >
+                <Send/>
+              </Button>
             </div>
-          ))}
-          <div ref={messagesEndRef} />
-      </Paper>
-        <div className='flex w-full space-x-4 p-4'>
-          <Input
-            placeholder="Describe injury or accident"
-            type="text"
-            value={input}
-            onChange={event => {
-              setInput(event.target.value);
-            }}
-            className='flex-1 mr-2'
-          />
-          <Button
-            variant="outlined"
-            className='bg-purple-400'
-            onClick={async () => {
-              const { messages, newMessage } = await continueConversation([
-                ...conversation,
-                { role: 'user', content: input },
-              ]);
-
-              let textContent = '';
-
-              for await (const delta of readStreamableValue(newMessage)) {
-                textContent = `${textContent}${delta}`;
-
-                setConversation([
-                  ...messages,
-                  { role: 'assistant', content: textContent },
-                ]);
-              }
-              setInput('')
-            }}
-          >
-            <Send/>
-          </Button>
-        </div>
-
+          </TabsContent>
+          <TabsContent value='illustration'>
+                <Illustrations/>
+          </TabsContent>
+        </Tabs>
     </div>
   );
 }
--- application/components/Footer.tsx
+++ application/components/Footer.tsx
@@ -1,10 +1,30 @@
+import { Facebook, Instagram, Twitter } from "@mui/icons-material";
 import { Paper } from "@mui/material";
+import Link from "next/link";
 
 export default function Footer() {
     return (
-        <Paper className="w-dvw md:max-w-6xl mx-auto p-6 h-[10rem] my-4 border-2 border-purple-500" elevation={3} >
-            <div className="grid items-center">
-                footer
+        <Paper className="w-dvw md:max-w-5xl mx-auto p-4 my-4 border-2 border-purple-500" elevation={3} >
+            <div className="grid items-center grid-cols-4 gap-4 font-medium text-gray-600">
+                <div className="flex flex-col items-center border-r-2 border-purple-500">
+                    <Link href="/">Home</Link>
+                    <Link href="/about">About</Link>
+                    <Link href="/contact">Contact</Link>
+                </div>
+                <div className="flex flex-col items-center border-r-2 border-purple-500">
+                    <Link href="/terms">Terms of Use</Link>
+                    <Link href="/privacy">Privay Policy</Link>
+                </div>
+                <div className="flex flex-col items-center border-r-2 border-purple-500">
+                <Link href="/services">Services</Link>
+                    <Link href="/resources">Resources</Link>
+                    <Link href="/newsletter">Newsletter</Link>
+                </div>
+                <div className="flex flex-col items-center">
+                    <Facebook sx={ {height: 30 , width: 30 , color: 'purple'}}/>
+                    <Twitter sx={ {height: 30 , width: 30 , color: 'purple'}}/>
+                    <Instagram sx={ {height: 30 , width: 30 , color: 'purple'}}/>
+                </div>
             </div>
         </Paper>
     )
