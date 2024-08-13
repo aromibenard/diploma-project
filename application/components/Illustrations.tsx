f8979f3462175d36af62faf5c95099e86c308e55
Benard Aromi <112564277+aromibenard@users.noreply.github.com>
2024-08-13 10:19:57 +0000

    adding illustration images. Presentations set for August 20th

--- application/components/Illustrations.tsx
+++ application/components/Illustrations.tsx
@@ -4,6 +4,7 @@ import {
     AccordionItem,
     AccordionTrigger,
   } from "@/components/ui/accordion"
+import Image from "next/image"
   
   export function Illustrations() {
     return (
@@ -11,19 +12,64 @@ import {
         <AccordionItem value="item-1">
           <AccordionTrigger>Lacerations</AccordionTrigger>
           <AccordionContent>
-            ..in development
+            <div className="h-[9rem] flex p-2 space-x-4 overflow-x-auto">
+              <Image 
+                src={'/laceration1.jpg'}
+                height={200}
+                width={200}
+                alt="Laceration"
+              />
+              <Image 
+                src={'/laceration2.jpg'}
+                height={200}
+                width={200}
+                alt="Laceration"
+              />
+              <Image 
+                src={'/bruises1.jpeg'}
+                height={200}
+                width={200}
+                alt="bruises"
+              />
+            </div>
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-2">
           <AccordionTrigger>Burns</AccordionTrigger>
           <AccordionContent>
-            ...in development
+          <div className="h-[9rem] flex p-2 space-x-4 overflow-x-auto">
+              <Image 
+                src={'/burns1.jpg'}
+                height={200}
+                width={200}
+                alt="burns"
+              />
+              <Image 
+                src={'/laceration1.jpg'}
+                height={200}
+                width={200}
+                alt="burns"
+              />
+            </div>
           </AccordionContent>
         </AccordionItem>
         <AccordionItem value="item-3">
           <AccordionTrigger>Cuts & Bruises</AccordionTrigger>
           <AccordionContent>
-            ...in development
+          <div className="h-[10rem] flex p-2 space-x-4 overflow-x-auto">
+              <Image 
+                src={'/bruises1.jpeg'}
+                height={200}
+                width={200}
+                alt="bruises"
+              />
+              <Image 
+                src={'/laceration2.jpg'}
+                height={200}
+                width={200}
+                alt="bruises"
+              />
+            </div>
           </AccordionContent>
         </AccordionItem>
       </Accordion>
